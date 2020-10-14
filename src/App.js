e90aea19ddf40ac2e2506d22ed79199942931d79
unknown <saqlainprinters@gmail.com>
2020-10-14 11:03:45 +0500

    Init

--- src/App.js
+++ src/App.js
@@ -1,26 +1,65 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import "./App.css";
+import "./counter.css";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends React.Component {
+  state = {
+    counter: 0,
+  };
+
+  animate = () => {
+    let counterA = document.querySelector(".counter");
+    counterA.style.animationName = "counter";
+    counterA.style.animationDuration = "150ms";
+    setTimeout(() => {
+      counterA.style.animation = "none";
+    }, 150);
+  };
+
+  add = () => {
+    this.animate();
+
+    this.setState({
+      counter: this.state.counter + 1,
+    });
+  };
+  sub = () => {
+    this.animate();
+    this.setState({
+      counter: this.state.counter - 1,
+    });
+  };
+  render() {
+    return (
+      <div className="containerWrapper">
+      <div className="container">
+        <h1 className="mainHeading">Counter App</h1>
+        <div className="counterContainer">
+          <div className="counter">{this.state.counter}</div>
+        </div>
+        <div className="btnContainer">
+          <button className="btn" onClick={this.add}>
+            +
+          </button>
+          <button className="btn" onClick={this.sub}>
+            -
+          </button>
+        </div>
+      </div>
+      </div>
+    );
+  }
 }
+
+// function App() {
+//   return (
+//     <div>
+//       <h1>Counter App</h1>
+//       <h2>0</h2>
+//       <button>+</button>
+//       <button>-</button>
+//     </div>
+//   );
+// }
 
 export default App;
